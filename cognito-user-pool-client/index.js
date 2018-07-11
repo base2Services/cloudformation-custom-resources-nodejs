c34c0fdda04149277288f3797e7e0799a756022f
toshke <n.tosic@base2services.com>
2018-07-11 15:02:35 +1000

    treat skip update as string

--- cognito-user-pool-client/index.js
+++ cognito-user-pool-client/index.js
@@ -4,96 +4,96 @@
  **/
 
 let AWS = require('aws-sdk'),
-  CfnLambda = require('cfn-lambda'),
-  CognitoApi = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: '2016-04-18'
-  });
+    CfnLambda = require('cfn-lambda'),
+    CognitoApi = new AWS.CognitoIdentityServiceProvider({
+        apiVersion: '2016-04-18'
+    });
 
 let logic = {
-  Create: CfnLambda.SDKAlias({
-    api: CognitoApi,
-    method: 'createUserPoolClient',
-    forceBools: [
-      'GenerateSecret',
-      'AllowedOAuthFlowsUserPoolClient'
-    ],
-    returnAttrs: [
-      'UserPoolClient.ClientId',
-      'UserPoolClient.CreationDate',
-      'UserPoolClient.LastModifiedDate',
-      'UserPoolClient.ClientSecret'
-    ],
-    returnPhysicalId: 'UserPoolClient.ClientId'
-  }),
+    Create: CfnLambda.SDKAlias({
+        api: CognitoApi,
+        method: 'createUserPoolClient',
+        forceBools: [
+            'GenerateSecret',
+            'AllowedOAuthFlowsUserPoolClient'
+        ],
+        returnAttrs: [
+            'UserPoolClient.ClientId',
+            'UserPoolClient.CreationDate',
+            'UserPoolClient.LastModifiedDate',
+            'UserPoolClient.ClientSecret'
+        ],
+        returnPhysicalId: 'UserPoolClient.ClientId'
+    }),
 
-  DoUpdate: CfnLambda.SDKAlias({
-    api: CognitoApi,
-    method: 'updateUserPoolClient',
-    returnAttrs: [
-      'UserPoolClient.ClientId',
-      'UserPoolClient.CreationDate',
-      'UserPoolClient.LastModifiedDate',
-      'UserPoolClient.ClientSecret'
-    ],
-    physicalIdAs: 'ClientId',
-    keys: [
-      'UserPoolId',
-      'AllowedOAuthFlows',
-      'AllowedOAuthFlowsUserPoolClient',
-      'AllowedOAuthScopes',
-      'CallbackURLs',
-      'ClientName',
-      'DefaultRedirectURI',
-      'ExplicitAuthFlows',
-      'LogoutURLs',
-      'ReadAttributes',
-      'RefreshTokenValidity',
-      'SupportedIdentityProviders',
-      'WriteAttributes',
-      'ClientId'
-    ],
-    forceBools: [
-      'AllowedOAuthFlowsUserPoolClient'
-    ],
-  }),
+    DoUpdate: CfnLambda.SDKAlias({
+        api: CognitoApi,
+        method: 'updateUserPoolClient',
+        returnAttrs: [
+            'UserPoolClient.ClientId',
+            'UserPoolClient.CreationDate',
+            'UserPoolClient.LastModifiedDate',
+            'UserPoolClient.ClientSecret'
+        ],
+        physicalIdAs: 'ClientId',
+        keys: [
+            'UserPoolId',
+            'AllowedOAuthFlows',
+            'AllowedOAuthFlowsUserPoolClient',
+            'AllowedOAuthScopes',
+            'CallbackURLs',
+            'ClientName',
+            'DefaultRedirectURI',
+            'ExplicitAuthFlows',
+            'LogoutURLs',
+            'ReadAttributes',
+            'RefreshTokenValidity',
+            'SupportedIdentityProviders',
+            'WriteAttributes',
+            'ClientId'
+        ],
+        forceBools: [
+            'AllowedOAuthFlowsUserPoolClient'
+        ],
+    }),
 
-  Delete: CfnLambda.SDKAlias({
-    api: CognitoApi,
-    method: 'deleteUserPoolClient',
-    keys: ['UserPoolId','ClientId'],
-    physicalIdAs: 'ClientId'
-  })
+    Delete: CfnLambda.SDKAlias({
+        api: CognitoApi,
+        method: 'deleteUserPoolClient',
+        keys: ['UserPoolId', 'ClientId'],
+        physicalIdAs: 'ClientId'
+    })
 };
 
 logic.NoUpdate = CfnLambda.SDKAlias({
-  api: CognitoApi,
-  method: 'describeUserPoolClient',
-  returnAttrs: [
-    'UserPoolClient.ClientId',
-    'UserPoolClient.CreationDate',
-    'UserPoolClient.LastModifiedDate',
-    'UserPoolClient.ClientSecret'
-  ],
-  physicalIdAs: 'ClientId',
-  keys: [
-    'UserPoolId',
-    'ClientId'
-  ]
+    api: CognitoApi,
+    method: 'describeUserPoolClient',
+    returnAttrs: [
+        'UserPoolClient.ClientId',
+        'UserPoolClient.CreationDate',
+        'UserPoolClient.LastModifiedDate',
+        'UserPoolClient.ClientSecret'
+    ],
+    physicalIdAs: 'ClientId',
+    keys: [
+        'UserPoolId',
+        'ClientId'
+    ]
 }),
 
-logic.Update = function(physicalId, params, oldParams, callback) {
-    if(params.SkipUpdate) {
-      return logic.NoUpdate(physicalId, params, callback);
-    } else {
-      return logic.DoUpdate(physicalId, params, oldParams, callback);
-    }
-};
+    logic.Update = function (physicalId, params, oldParams, callback) {
+        if (params.SkipUpdate == 'true') {
+            return logic.NoUpdate(physicalId, params, callback);
+        } else {
+            return logic.DoUpdate(physicalId, params, oldParams, callback);
+        }
+    };
 
 exports.handler = CfnLambda({
-  Create: logic.Create,
-  Update: logic.Update,
-  Delete: logic.Delete,
-  NoUpdate: logic.NoUpdate,
-  TriggersReplacement: ['UserPoolId'],
-  SchemaPath: [__dirname, 'schema.json']
+    Create: logic.Create,
+    Update: logic.Update,
+    Delete: logic.Delete,
+    NoUpdate: logic.NoUpdate,
+    TriggersReplacement: ['UserPoolId'],
+    SchemaPath: [__dirname, 'schema.json']
 });
